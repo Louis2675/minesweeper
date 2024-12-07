23eb238d7a26f00de126e6d18e05ee10be876264
Louis Declerck <84902823+Louis2675@users.noreply.github.com>
2024-12-07 04:01:08 +0100

    did a wee more here

--- 1ere/demineur.py
+++ 1ere/demineur.py
@@ -1,3 +1,21 @@
 """
-
+Fichier principal du jeu de démineur, il permet de demarrer la partie
 """
+
+from noyau import *
+from affichage import *
+from parametres import *
+from initialisation import *
+
+def jouer_au_demineur():
+    """
+    Fonction principale du jeu de démineur
+    """
+    print("Bienvenue dans le jeu de démineur !")
+    continuer = True
+    while continuer:
+        print("Nouvelle partie")
+        continuer = input("Voulez-vous continuer ? (o/n) : ") == "o"
+        grille = generer_grilles(TAILLE_GRILLE)
+        saisir_bombes(grille, generer_bombes(NB_MINES, TAILLE_GRILLE))
+        
